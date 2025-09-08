ae438a68bccc6be3704a67ee9a7faeb94eea96d4
Indal Yadav <indalkumar56@gmail.com>
2025-09-08 17:16:47 +0530

    monitoring added

--- grpc/swagger-api-service/go.mod
+++ grpc/swagger-api-service/go.mod
@@ -5,6 +5,7 @@ go 1.24.5
 require (
 	github.com/MarceloPetrucio/go-scalar-api-reference v0.0.0-20240521013641-ce5d2efe0e06
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-chi/cors v1.2.2
 	github.com/swaggo/http-swagger v1.3.4
 )
 
--- grpc/swagger-api-service/main.go
+++ grpc/swagger-api-service/main.go
@@ -10,17 +10,27 @@ import (
 
 	"github.com/MarceloPetrucio/go-scalar-api-reference"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func main() {
-	combineSwagger()
+	// combineSwagger()
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// health check endpoint
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -65,4 +75,24 @@ func combineSwagger() {
 	if err != nil {
 		log.Fatalf("Error combining swagger: %v", err)
 	}
+}
+
+
+// CORS middleware
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // allow all origins
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		// Pass to the next handler
+		next.ServeHTTP(w, r)
+	})
 }
